520314cbe467638be3dd1ea59f357afc86f8e873
Adam Jolicoeur <mindreeper2420@users.noreply.github.com>
2017-06-29 11:10:20 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git init
 git config user.name "Adam Jolicoeur"
 git config user.email "adam.jolicoeur@gmail.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/fabric8io/fabric8-ux.git"
+git remote add upstream "https://$GH_TOKEN@github.com/fabric8-ui/fabric8-ux.git"
 git fetch upstream
 git reset upstream/gh-pages
 
