d63ea8e0e400cc7312da2e9e9e10193f3f984f4e
Adam Jolicoeur <adam.jolicoeur@redhat.com>
2017-12-06 17:19:40 -0500

    feat(upgrade): Upgrade PatternFly and clean site structure
    Update to PatternFly 3.31.0 with SASS in Core; clean site structure of duplicates

--- gulpfile.js
+++ gulpfile.js
@@ -9,26 +9,26 @@ gulp.task('clean', function() {
 
 // Copy libraries from /node_modules to their respective locations
 gulp.task('copyJS', function() {
-    gulp.src(['node_modules/bootstrap-sass/assets/javascripts/*.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/jquery/dist/jquery.min.map', 'node_modules/patternfly-sass/assets/javascripts/**'])
-        .pipe(gulp.dest('js/vendor/'))
+    gulp.src(['node_modules/bootstrap-sass/assets/javascripts/*.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/jquery/dist/jquery.min.map', 'node_modules/patternfly/dist/js/*.min.js'])
+        .pipe(gulp.dest('assets/js/vendor/'))
 });
 
 gulp.task('copySASS', function() {
-    gulp.src(['node_modules/patternfly-sass/assets/stylesheets/**'])
-        .pipe(gulp.dest('_sass/vendor/patternfly/'))
+    gulp.src(['node_modules/patternfly/dist/sass/**'])
+        .pipe(gulp.dest('_sass/patternfly/'))
     gulp.src(['node_modules/bootstrap-sass/assets/stylesheets/bootstrap/**'])
-        .pipe(gulp.dest('_sass/vendor/patternfly/bootstrap/'))
+        .pipe(gulp.dest('_sass/patternfly/bootstrap/'))
     gulp.src(['node_modules/font-awesome/scss/**'])
-        .pipe(gulp.dest('_sass/vendor/patternfly/'))
+        .pipe(gulp.dest('_sass/patternfly'))
 });
 
 gulp.task('copyFonts', function() {
     gulp.src(['node_modules/bootstrap-sass/assets/fonts/**'])
-        .pipe(gulp.dest('fonts'))
-    gulp.src(['node_modules/patternfly-sass/assets/fonts/patternfly/**'])
-        .pipe(gulp.dest('fonts/patternfly'))
+        .pipe(gulp.dest('assets/fonts/'))
+    gulp.src(['node_modules/patternfly/dist/fonts/**'])
+      .pipe(gulp.dest('assets/fonts/'))
     gulp.src(['node_modules/font-awesome/fonts/**'])
-        .pipe(gulp.dest('fonts/'))
+      .pipe(gulp.dest('assets/fonts/'))
 });
 
 gulp.task('cleanVendors', function() {
